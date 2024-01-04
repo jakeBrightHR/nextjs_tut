72202aeb2c08c9fd563fb74bafa18e0f02f61843
jakeBrightHR <jake.kinglee@brighthr.com>
2024-01-04 15:45:39 +0000

    chapter 12

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,16 +1,17 @@
+import { createInvoice } from '@/app/lib/actions';
 import { CustomerField } from '@/app/lib/definitions';
-import Link from 'next/link';
+import { Button } from '@/app/ui/button';
 import {
   CheckIcon,
   ClockIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
-import { Button } from '@/app/ui/button';
+import Link from 'next/link';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -70,7 +71,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   name="status"
                   type="radio"
                   value="pending"
-                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  className="h-8 w-8 cursor-pointer border-gray-300 bg-gray-100 text-gray-500 focus:ring-2"
                 />
                 <label
                   htmlFor="pending"
